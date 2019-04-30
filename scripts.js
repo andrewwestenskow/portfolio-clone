1406715c809c83e47eaa2281559532b2142508cc
Andrew Westenskow <andrew.westenskow@gmail.com>
2019-04-29 18:06:39 -0600

    added styles

--- scripts.js
+++ scripts.js
@@ -8,3 +8,20 @@ function handleScroll() {
   }
 
 }
+
+function handleMenu() {
+  if(mobileMenu.classList.contains('hidden-menu')){
+    mobileMenu.classList.add('showing-menu')
+    mobileMenu.classList.remove('hidden-menu')
+  } else if(mobileMenu.classList.contains('showing-menu')){
+    mobileMenu.classList.add('hidden-menu')
+    mobileMenu.classList.remove('showing-menu')
+  }
+}
+
+const mobileMenus = document.getElementsByClassName('mobile-menu')
+const mobileMenu = mobileMenus[0]
+const mobileMenuButtons = document.getElementsByClassName('menu')
+const mobileMenuButton = mobileMenuButtons[0]
+
+mobileMenuButton.addEventListener('click', handleMenu)
